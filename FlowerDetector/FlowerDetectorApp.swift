60c35ec13c0841f271c0ba844807e381ae61f40b
jwilson <jwilsonrae@gmail.com>
2024-07-19 14:19:13 +0200

    Init commit // project boilerplate

---
+++ FlowerDetector/FlowerDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowerDetectorApp.swift
+//  FlowerDetector
+//
+//  Created by Joaquin Wilson on 19-07-24.
+//
+
+import SwiftUI
+
+@main
+struct FlowerDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
